2bd0055daa5571db572c46377429ba1ffdbcafe1
Prathibha Ratnayake <prsthibha@gmail.com>
2025-05-26 20:43:12 +0530

    fix(auth): fix form width

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -4,7 +4,7 @@ import { RegisterForm } from '@/components/auth/register-form';
 
 export default async function RegisterPage() {
   return (
-    <div className='flex min-h-screen items-center justify-center px-4 py-12'>
+    <div className='flex min-h-screen min-w-screen items-center justify-center px-4 py-12'>
       <RegisterForm />
     </div>
   );
